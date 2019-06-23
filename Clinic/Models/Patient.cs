90e17763c8838f462e19ff0c5963c12239abac38
John Edison Restrepo Bedoya <jovento@gmail.com>
2019-06-23 11:44:26 -0500

    Initial commit, project creation. Added Patient class

---
+++ Clinic/Models/Patient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Clinic.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Phone { get; set; }
+
+    }
+}
