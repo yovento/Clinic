cfb37b34fe29fbcbc71fd7576d9a55ac1c2c3485
John Edison Restrepo Bedoya <jovento@gmail.com>
2019-06-23 16:26:12 -0500

    Included datatables and listing of patients

--- Clinic/App_Start/BundleConfig.cs
+++ Clinic/App_Start/BundleConfig.cs
@@ -11,6 +11,10 @@ namespace Clinic
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+                        "~/Scripts/datatables/jquery.datatables.js",
+                        "~/Scripts/datatables/datatables.bootstrap.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -25,7 +29,8 @@ namespace Clinic
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/datatables/css/datatables.bootstrap.css"));
         }
     }
 }
