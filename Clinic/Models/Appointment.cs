96e6c8a498f5cc558c66fd7994f1e92a1ad1b527
John Edison Restrepo Bedoya <jovento@gmail.com>
2019-06-23 11:50:03 -0500

    Added Appointment Class and Annotations

---
+++ Clinic/Models/Appointment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Clinic.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public Patient Patient { get; set; }
+
+        [Required]
+        public int PatientId { get; set; }
+
+        [Required]
+        public DateTime AppointmentDate { get; set; }
+
+        [Required]
+        public bool IsActiveAppointment { get; set; }
+
+    }
+}
